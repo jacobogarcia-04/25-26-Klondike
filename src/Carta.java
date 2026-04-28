c79720c5a56beb572ff500d25dbd140ed3a9561c
manuel@-2.6 <manuel@masiasweb.com>
2026-04-28 14:06:46 +0200

    Avance : clase Baraja y clase Carta auxiliar

---
+++ src/Carta.java
@@ -0,0 +1,12 @@
+public class Carta {
+
+    public Carta(int palo, int numero) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public void mostrar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
+    }
+
+}
