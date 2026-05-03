0fafeb4697d473bcc898c500953b878fa9105299
jacobogarcia-04 <jacobo.garcia@alumnos.uneatlantico.es>
2026-05-03 12:18:51 +0200

    feat : Logica implementada en la clase palo.

--- src/Palo.java
+++ src/Palo.java
@@ -1,12 +1,16 @@
 public class Palo {
+
     private Carta[] carta;
     private Console console;
     private int ultima;
+    private String nombre;
+    private final int NUMERO_CARTAS_MAXIMAS = 13;
 
-    public Palo() {
+    public Palo(String nombresPalos) {
         carta = new Carta[13];
         console = new Console();
         ultima = 0;
+        this.nombre = nombresPalos;
     }
 
     private boolean estaVacio() {
@@ -15,18 +19,34 @@ public class Palo {
 
     public void mostrar() {
 
+        if (this.estaVacio()) {
+            console.writeln(nombre + "[]");
+        } else {
+            Carta cima = carta[ultima - 1];
+            console.write("Palo:" + nombre + "->");
+            cima.mostrar();
+        }
     }
 
     public void moverA(Columna columnaA) {
-
+        if (!this.estaVacio()) {
+            Carta cima = this.carta[ultima - 1];
+            if (columnaA.esApilable(cima)) {
+                columnaA.añadir(cima);
+                ultima--;
+            }
+        }
     }
 
     public boolean completo() {
-
+        return ultima == NUMERO_CARTAS_MAXIMAS;
     }
 
-    public void añadir(Carta cartaCima) {
-
+    public void añadir(Carta cartaNueva) {
+        if (this.esApilable(cartaNueva)) {
+            this.carta[ultima] = cartaNueva;
+            ultima++;
+        }
     }
 
     public boolean esApilable(Carta cartaNueva) {
